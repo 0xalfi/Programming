7f0d0b87560799585bbdcee584a5da6e18384c6c
0xalfi <70999423+0xalfi@users.noreply.github.com>
2023-03-03 21:54:28 +0700

    Create tugas1.cpp

---
+++ C++/praktek/tugas1.cpp
@@ -0,0 +1,16 @@
+// 03032023
+// Praktek Mendeklarasikan variabel dan mengisinya
+// Praktek mengimplementasikan operator pengurangan di c++
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int bilangan1 = 20;
+    int bilangan2 = 10;
+    cout << bilangan1 - bilangan2;
+    
+    cout << endl;
+   
+}
